6502ce6e2862fdb22b9d5487bb441f28405ad2a7
lloydagola <lloydagola@gmail.com>
2022-04-10 11:17:56 +0300

    created search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface Props{
+  searchTerm: string
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>
+}
+
+
+const SearchBar : React.FC<Props> = ({searchTerm = '', setSearchTerm}) => {
+  const placeholder:string = 'Search Github Users'
+  
+  return <input 
+              type='search' 
+              placeholder={placeholder} 
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onBlur={() => console.log(`search for ${searchTerm}?`)}
+          />
+}
+
+const App:React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('')
+
   return (
     <div className="App">
+      <SearchBar  searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
      
     </div>
   );
