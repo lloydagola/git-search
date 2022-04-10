24475acdb60dca5a42330a569b68aaaba58d5e17
lloydagola <lloydagola@gmail.com>
2022-04-10 11:33:51 +0300

    created component to render results

--- src/components/autocompletePanel/AutocompletePanel.tsx
+++ src/components/autocompletePanel/AutocompletePanel.tsx
@@ -8,12 +8,22 @@ interface Props{
     results: User[]
 }
 
+interface UserProps{
+    user: User
+}
+
+const Result = ({user}:UserProps) => {
+
+    return <Link to={`/results/${user.login}`} key={user.id}><p>{user.login}</p></Link>
+
+}
+
 const AutocompletePanel = ({results = []}:Props) => {
     
     const renderUsers = () => {
 
         return<>
-            {results.length> 0 && results.slice(0, 10).map((user:User) => <Link to={`/results/${user.login}`} key={user.id}><p>{user.login}</p></Link>)}
+            {results.length> 0 && results.slice(0, 10).map((user:User) => <Result user={user}/>)}
             {results.length < 1 && <>0</>}
         </>
     }
