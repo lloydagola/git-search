7c5469012111779f036c4ff40687ffdfc23f3144
lloydagola <lloydagola@gmail.com>
2022-04-10 11:21:35 +0300

    added components for handling errors and loading states

---
+++ src/components/errorComponent/ErrorComponent.tsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react"
+//models
+
+
+
+const ErrorComponent = () => {
+    
+    const renderUsers = () => <p>Users</p>
+    
+   
+
+}
+
+export default ErrorComponent
